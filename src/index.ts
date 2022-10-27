6b6eb89ee5fa4c52b309433854c0b96b9b7530df
Alejandro.Sanchez <alejandro.sanchez@comafi.com.ar>
2022-10-27 13:46:48 -0300

    Map Operator

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { map, range } from "rxjs";
+import { fromEvent, map, range } from "rxjs";
 
-range(1, 5)
-  .pipe(map<number, number>((value) => value * 10))
-  .subscribe(console.log);
+// range(1, 5)
+//   .pipe(map<number, number>((value) => value * 10))
+//   .subscribe(console.log);
+
+const keyUp$ = fromEvent<KeyboardEvent>(document, "keyup");
+
+const keyLetters = keyUp$.pipe(map((event) => event.key));
+
+keyLetters.subscribe(console.log);
